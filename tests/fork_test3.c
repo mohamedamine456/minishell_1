84c5c0d919a3a6eab56dd94a7d94a6b9ceaffd54
mohamedamine456 <aminelachheb456@gmail.com>
2021-02-10 18:30:24 +0100

    load

--- tests/fork_test3.c
+++ tests/fork_test3.c
@@ -1,7 +1,15 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
 
 int main()
 {
+	pid_t pid;
 
+	//printf("%lu", sizeof(pid_t));
+	pid = fork();
+	if (pid == 0)
+		printf("Hello from child\n");
+	else
+		printf("Hello from parent\n");
 }
