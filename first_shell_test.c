94b4384dc2b978b387de8d8e107007727ceae9d3
mohamedamine456 <aminelachheb456@gmail.com>
2021-02-16 08:45:01 +0100

    truncat

--- first_shell_test.c
+++ first_shell_test.c
@@ -2,19 +2,33 @@
 
 void		first_shell_test()
 {
-	char *str;
+	char	*buffer;
+	char	*str;
+	int		i;
+	int		j;
 
 	//str = malloc(100);
 	write(1, "*_*_*_*_*_*_* Welcome to minishell *_*_*_*_*_*_*\n", 49);
 	write(1, "MiniShell $> ", 13);
-	while (get_next_line(0, &str))
+	while (get_next_line(0, &buffer))
 	{
-		if (!ft_strncmp(str, "exit", ft_strlen(str)))
+		if (!ft_strncmp(buffer, "exit", ft_strlen(buffer)))
 			exit(0);
-		write(1, "\n", 1);
-		write(1, str, ft_strlen(str));
-		write(1, "\nMiniShell $> ", 14);
-		
+		else
+		{
+			i = 0;
+			while (buffer[i] != '\0')
+			{
+				j = i;
+				while (buffer[i] != ' ' && buffer[i] != '\0')
+					i++;
+				str = ft_substr(buffer, j, i - j);	
+				write(1, str, ft_strlen(str));
+				write(1, "\n", 1);
+				i++;
+			}
+		}
+		write(1, "\nMiniShell $> ", 14);	
 	}
 	free(str);
 }
