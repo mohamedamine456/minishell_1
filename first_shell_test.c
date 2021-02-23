bf226b7d85b7084def0396261f414ddfd14dd2fc
mohamedamine456 <aminelachheb456@gmail.com>
2021-02-23 12:19:56 +0100

    no quotes

--- first_shell_test.c
+++ first_shell_test.c
@@ -6,42 +6,12 @@
 /*   By: mlachheb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 11:39:33 by mlachheb          #+#    #+#             */
-/*   Updated: 2021/02/18 12:09:05 by mlachheb         ###   ########.fr       */
+/*   Updated: 2021/02/23 12:09:17 by mlachheb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int			check_quotes(char *buffer)
-{
-	int 	i;
-	int		quotes;
-
-	i = 0;
-	quotes = 0;
-	while (buffer[i] != '\0')
-	{
-		if (buffer[i] == '\"')
-			quotes++;
-		i++;
-	}
-	if ((quotes % 2) == 0)
-		return (1);
-	else
-		return (0);
-}
-
-char		*add_quotes(char *buffer)
-{
-	char	*str;
-
-	write(1, "> ", 2);
-	get_next_line(1, &str);
-	buffer = ft_strjoin(buffer, str);
-	if (!check_quotes(buffer))
-		buffer = add_quotes(buffer);
-	return (buffer);	
-}
 
 void		first_shell_test()
 {
@@ -55,8 +25,6 @@ void		first_shell_test()
 	write(1, "MiniShell $> ", 13);
 	while (get_next_line(0, &buffer))
 	{
-		if (!check_quotes(buffer))
-			buffer = add_quotes(buffer);	
 		if (!ft_strncmp(buffer, "exit", ft_strlen(buffer)))
 			exit(0);
 		else
