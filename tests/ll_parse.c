bfd3cf22441633ec4d464f44d0bdbc86c4b85d08
mohamedamine456 <aminelachheb456@gmail.com>
2021-03-01 17:48:07 +0100

    enum and parser tests

---
+++ tests/ll_parse.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+int main()
+{
+	int		i;
+	char	*buffer;
+
+	buffer = strcpy(buffer, "(hello, world)");
+	while(buffer[i] != '\0')
+	{
+
+		i++;
+	}
+	return (0);
+}
